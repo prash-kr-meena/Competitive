b75ece4588b0ec8149a07fd5b5ef68836232d6e7
prashant_Meena <prashantkr314@gmail.com>
2017-06-04 21:22:12 +0530

    preparing

--- cHelper/src/codeforces/Theatre_Square.java
+++ cHelper/src/codeforces/Theatre_Square.java
@@ -3,7 +3,8 @@ package codeforces;
 import java.util.Scanner;
 import java.io.PrintWriter;
 
-public class TaskA {
+public class Theatre_Square {
     public void solve(int testNumber, Scanner in, PrintWriter out) {
+
     }
 }
