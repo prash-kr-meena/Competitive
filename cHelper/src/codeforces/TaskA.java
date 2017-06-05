e92247e3aca34071c9c1ad41bc4c0b1a75f4c90e
prashant_Meena <prashantkr314@gmail.com>
2017-06-05 08:05:54 +0530

    preparing

---
+++ cHelper/src/codeforces/TaskA.java
@@ -0,0 +1,19 @@
+package codeforces;
+
+import FastIO.InputReader;
+import FastIO.OutputWriter;
+
+public class TaskA {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+
+        int T = in.nextInt();
+        for (int i = 0; i < T; i++) {
+
+            String s = in.readString();
+
+            out.printLine(s.length());
+
+        }
+
+    }
+}
